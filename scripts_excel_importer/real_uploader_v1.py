952b32e324ea583631c601d2ab9f46dcf3daf6b0
gdrillaud@asmodine.com <gdrillaud@asmodine.com>
2020-01-06 11:12:11 +0100

    modifications from Guillaume D

--- scripts_excel_importer/real_uploader_v1.py
+++ scripts_excel_importer/real_uploader_v1.py
@@ -3,14 +3,13 @@ import csv
 import os
 from os import listdir
 from os.path import isfile, join
-from pandas import DataFrame, read_csv, ExcelWriter
 import pandas as pd
-import xlwt
-import xlrd
-from xlwt import Workbook 
 import re
-import time
 from collections import defaultdict
+import numpy as np
+import xlrd
+import xlwt
+from shutil import copyfile
 
 class Sql:
 
@@ -34,159 +33,157 @@ class Sql:
 	def connectorwamp(self):
 		self.mydb = mysql.connector.connect(
 			host="localhost",
-			user="root",
-			passwd="",
-			database="exo"
+			user="phpasmopim",
+			passwd="changeme",
+			database="dbasmodinepim",
+			port=3332
 			)
 		self.mycursor = self.mydb.cursor()
 		self.table_name = "pim_brand_size"
 
-	def hidden_remover(self):
-		with open("failed_files" + ".csv", "a", newline='\n') as fp:
-			a = csv.writer(fp)
-			row = ['FILE NAME'] + ['ERROR CODE'] + ['ERROR EXPLAINED'] 
-			a.writerow(row)
-		with open('codelist.txt', 'r') as f:
-			self.identifiants = f.read().splitlines()
-		with open('namelist.txt', 'r') as f:
-			self.names = f.read().splitlines()
+
+	def getSizesBrand(self):
+		sql = "SELECT name FROM pim_brand_size"
+		self.mycursor.execute(sql)
+		myresult = self.mycursor.fetchall()
+		sizes = [item[0] for item in myresult]
+		return sizes
+
+	def remove_hidden_rows(self):
 		self.files = [f for f in listdir('./files/') if isfile(join('./files/', f))]
 		for self.file in self.files:
-			self.match = re.search(r'\d{7}', self.file)
-			df = pd.read_excel('./files/' + self.file, sheet_name='BAREME-MEASUREMENT CHART', header=1)
-			indexx = 1
-			suppresor = 0
-			col_suppresor = 0
-			while str(df.columns[0]).count('TAILLES') == 0:
-				suppresor += 1
-				indexx += 1
-				df = pd.read_excel('./files/' + self.file, sheet_name='BAREME-MEASUREMENT CHART', header=indexx)
-
-			while str(df.columns[-1]).count('Unnamed') > 0:
-				df = df.drop(df.columns[-1], axis=1)
-
-			if str(df.columns[2]).count('Unnamed') > 0:
-				df = df.drop(df.columns[2], axis=1)
-				col_suppresor += 1
+			print(self.file)
 			try:
 				wb = xlrd.open_workbook('./files/' + self.file, formatting_info=1)
 			except Exception as e:
 				print('xlxs file, should be xls file')
-				file_error = 'FILE : ' + str(self.file) + ' ERROR CODE : ' +  str(e) + ' ERREUR : ' + 'Wrong file extension, should be xls (not xlxs)'
+				file_error = 'FILE : ' + str(self.file) + ' ERROR CODE : ' + str(
+					e) + ' ERREUR : ' + 'Wrong file extension, should be xls (not xlxs)'
 				self.failed_file.append(file_error)
 				with open("failed_files" + ".csv", "a", newline='\n') as fp:
 					a = csv.writer(fp)
-					row = [str(self.file)] + [str(e)] + ['Wrong file extension, should be xls (not xlxs)'] 
+					row = [str(self.file)] + ['Wrong file extension, should be xls (not xlxs)']
 					a.writerow(row)
-				continue
-			ws = wb.sheet_by_index(0)
-			self.hidden_rows[self.file] = []
+				copyfile("files/" + self.file, "failed_files/xlxsFiles/" + self.file)
+
+			wb_sheet = wb.sheet_by_index(0)
+
+			newBook = xlwt.Workbook()
+			newSheet = newBook.add_sheet("data")
+
+			idx = 0
 			try:
-				for rowx in range(ws.nrows):
-					if ws.rowinfo_map[rowx].hidden > 0:
-						self.hidden_rows[self.file].append(rowx-2-suppresor)
+				for row_idx in range(0, wb_sheet.nrows):
+					hidden = wb_sheet.rowinfo_map[row_idx].hidden
+					if (hidden == 0):
+						for col_index, cell_obj in enumerate(wb_sheet.row(row_idx)):
+							newSheet.write(idx, col_index, cell_obj.value)
+						idx = idx + 1
+				newBook.save('./files/noHiddenRows/' + self.file)
 			except:
-				print('index bug')
+				copyfile("files/" + self.file, "failed_files/wentWrong/" + self.file)
 
-			for colx in range(ws.ncols):
-				if ws.colinfo_map[colx].hidden > 0:
-					self.hidden_cols[self.file].append(colx - col_suppresor)
+				print("Something went wrong")
 
-			df = df.drop(df.index[self.hidden_rows[self.file]])
-			df = df.drop(df.columns[self.hidden_cols[self.file]], axis=1)	
-			df.rename({df.columns[1]: 'code'}, axis=1, inplace=True)
-			if str(df.columns[2]).count('38/40') >0:
-				df.rename({df.columns[2]: '38/40'}, axis=1, inplace=True)
-			elif str(df.columns[2]).count('\n38') >0:
-				df.rename({df.columns[2]: '38'}, axis=1, inplace=True)
-			else:
-				print('ALERT')
+	def clean_data(self):
+		self.mycursor.execute("TRUNCATE TABLE pim_sizeguide_product")
+		with open("failed_files" + ".csv", "w", newline='\n') as fp:
+			a = csv.writer(fp)
+			row = ['FILE NAME'] + ['ERROR EXPLAINED']
+			a.writerow(row)
+		dfCodes = pd.read_csv("codeName.csv")
+		self.files = [f for f in listdir('./files/noHiddenRows/') if isfile(join('./files/noHiddenRows/', f))]
+		for self.file in self.files:
+			self.columnsMens = []
+			print(self.file)
 
-			df = df.iloc[:, :-3]
-			df['id'] = self.file
-			df.drop
-			df.set_index('id', drop=True, inplace=True)
-			self.df = df[df['code'].astype(str).str.contains('|'.join(self.identifiants), na=False) ]
-			while str(self.df.columns[-1]).count('Unnamed') > 0:
-				self.df = self.df.drop(self.df.columns[-1], axis=1)
-			sql_class.sql_uploader()
+			self.match = re.search(r'\d{7}', self.file)
+			sizesBrand = self.getSizesBrand()
+			df = pd.read_excel('./files/noHiddenRows/' + self.file, sheet_name='data', header=1)
+			self.df = df
+			df.columns.values[0] = "description"
+			df.columns.values[1] = "code"
+			columnsNames = ["description", "code"]
+			sizes = []
+			for columnName in df.columns:
+				columnNameCleaned = str(columnName).replace('\n', "").replace("BASIC", "").replace("SIZE", "").strip()
+
+				df = df.rename(columns={columnName: columnNameCleaned})
+				if columnNameCleaned in sizesBrand:
+					sizes.append(columnNameCleaned)
+					columnsNames.append(columnNameCleaned)
+
+			df = df[columnsNames]
+			df = df[df['code'].astype(str).str.contains('|'.join(list(dfCodes["code"])), na=False)]
+			try:
+				for size in sizes:
+					df[size] = df[size] * np.where(df['description'].astype(str).str.contains('1/2', na=False), 20, 10)
+				df = df.drop(columns=["description"])
+				df = df.set_index("code").T
+				df = df.groupby(level=0, axis=1).max()
+				for columnName in df.columns:
+					str(columnName).strip()
+					if columnName in self.getAllCodeNames():
+						df = df.rename(columns={columnName: dfCodes.loc[dfCodes['code'] == columnName]["name"].values[0]})
+					else:
+						df = df.drop([columnName], axis=1)
+				if len(df.columns) == len(set(df.columns)):
+					copyfile("files/" + self.file, "failed_files/duplicatesRow/" + self.file)
+
+				df = df.reset_index()
+				df = df.rename(columns={"index": "size"})
+				df = df.dropna(axis=1, how='all')
+				df = df.dropna(axis=0, thresh=2)
+				for columnName in df.columns:
+					if columnName not in ["size", "model_number", "code"]:
+						self.columnsMens.append(columnName)
+				try:
+					df["model_number"] = self.match.group()
+				except Exception as e:
+					file_error = 'FILE : ' + str(self.file) + ' ERREUR : ' + 'Pas d\'identifiant dans le nom du fichier'
+					self.failed_file.append(file_error)
+					with open("failed_files" + ".csv", "a", newline='\n') as fp:
+						a = csv.writer(fp)
+						row = [str(self.file)] + ['Pas d\'identifiant dans le nom du fichier']
+						a.writerow(row)
+						copyfile("files/"+self.file, "failed_files/noID/"+self.file)
+				else:
+					self.df = df
+					sql_class.sql_uploader()
+					print(self.df)
+			except:
+				copyfile("files/" + self.file, "failed_files/wentWrong/" + self.file)
+				print("Something went wrong")
+
+
+	def getAllCodeNames(self):
+		dfCodeNames = pd.read_csv("codeName.csv")
+		allCodenames = dfCodeNames["code"].tolist()
+		return allCodenames
+
 
 	def sql_uploader(self):
-		for i in range(len(self.df.index)):
-			for n in range(1, len(self.df.columns)):
-				identi = "SELECT id FROM pim_product WHERE model_id IN (SELECT id FROM pim_model WHERE model_number=%s) AND brand_size_id IN (SELECT id FROM pim_brand_size WHERE name=%s)"
+		for index, row in self.df.iterrows():
+			sql = "SELECT id FROM pim_product WHERE model_id IN (SELECT id FROM pim_model WHERE model_number=" + row["model_number"] + ") AND brand_size_id IN (SELECT id FROM pim_brand_size WHERE name LIKE '%" + str(row["size"]) + "%')"
+			self.mycursor.execute(sql)
+			myresult = self.mycursor.fetchall()
+			for result in range(len(myresult)):
+
+				sql = "INSERT INTO pim_sizeguide_product (product_id, "+','.join(self.columnsMens)+", created_at, updated_at) VALUES (%s, %s, %s"
+				val = [myresult[result][0]]
+				for men in self.columnsMens:
+					sql += ',%s'
+					val.append(row[men])
+				val.append('2019-12-23')
+				val.append('2019-12-23')
+				val = tuple(val)
+				sql += ')'
 				try:
-					self.args = (str(self.match.group()), str(self.df.columns[n]))
-				except Exception as e:
-					print(self.file)
-					print('No file name found')
-					try:
-						if self.failed_file[-1].count(str(self.file)) > 0:
-							print('error already added to array')
-						else:
-							file_error = 'FILE : ' + str(self.file) + ' ERROR CODE : ' +  str(e) + ' ERREUR : ' + 'Part of file name with 7 numbers not found'
-							self.failed_file.append(file_error)
-							with open("failed_files" + ".csv", "a", newline='\n') as fp:
-								a = csv.writer(fp)
-								row = [str(self.file)] + [str(e)] + ['Part of file name with 7 numbers not found'] 
-								a.writerow(row)
-						continue
-					except IndexError:
-						file_error = 'FILE : ' + str(self.file) + ' ERROR CODE : ' +  str(e) + ' ERREUR : ' + 'Part of file name with 7 numbers not found'
-						self.failed_file.append(file_error)
-						with open("failed_files" + ".csv", "a", newline='\n') as fp:
-							a = csv.writer(fp)
-							row = [str(self.file)] + [str(e)] + ['Part of file name with 7 numbers not found'] 
-							a.writerow(row)
-						continue
-
-				self.mycursor.execute(identi, self.args)
-				myresult = self.mycursor.fetchall()
-				try:
-					index_namelist = self.identifiants.index(str(self.df.iloc[i][self.df.columns[1]]))
-				except Exception as e:
-					print('index issue between filename.txt and codename.txt')
-					print(e)
-
-					continue
-				if len(myresult) > 0:
-					pass
-				else:
-					print("no product_id found")
-					continue
-				if str(self.df.iloc[i][self.df.columns[0]]).count('1/2') > 0:
-					multiplier = 20
-				else:
-					multiplier = 10
-
-				for result in range(len(myresult)):
-					try:
-						sql = "INSERT INTO pim_sizeguide_product (product_id, {0}, created_at, updated_at) VALUES (%s,%s, %s, %s)".format(str(self.names[index_namelist]))
-						val = (
-							myresult[result][0],
-							str(self.df.iloc[i][self.df.columns[n]]*multiplier),
-							'2019-08-12',
-							'2019-08-12',
-							)
-						self.mycursor.execute(sql, val)
-						self.mydb.commit()
-						print('send to db successful')
-
-					except mysql.connector.IntegrityError as e:
-						sql = "UPDATE pim_sizeguide_product SET {0} = {1} WHERE product_id={2}".format(str(self.names[index_namelist]), str(self.df.iloc[i][self.df.columns[n]]*multiplier), myresult[result][0])
-						self.mycursor.execute(sql)
-						self.mydb.commit()
-						print('send to db successful')
-
-					except mysql.connector.DatabaseError as e:
-						print('error : Nan value not sent to DB')
-						print(e)
-						continue
-
-					except ValueError as e:
-						print('value error')
-						print(e)
-						continue
+					self.mycursor.execute(sql, val)
+					self.mydb.commit()
+					print('send to db successful')
+				except:
+					print('send to db not successful')
 
 
 	def ender(self):
@@ -205,5 +202,6 @@ class Sql:
 if __name__ == '__main__':
 	sql_class = Sql()
 	sql_class.connectorwamp()
-	sql_class.hidden_remover()	
+	#sql_class.remove_hidden_rows()
+	sql_class.clean_data()
 	sql_class.ender()
